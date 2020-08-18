6f8597ae9d4c97e1e6f0f86bbe774722af637a5a
hmaag <maag.heath@gmail.com>
2020-08-17 22:27:15 -0700

    Add user profile editing
    This commit gives users the ability to edit their profile.
    Small css and message changes were made as well.

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
@@ -12,3 +13,24 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+        self.fields['email'].label = "email"
+        self.fields['email'].required = False
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProfileUpdateForm, self).__init__(*args, **kwargs)
+        self.fields['about'].label = "about"
+
+    class Meta:
+        model = Profile
+        fields = ['about']
