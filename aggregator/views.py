f9fc1170500875a30a6fcb65feae6893650792b0
hmaag <maag.heath@gmail.com>
2020-08-18 22:23:18 -0700

    Add pagination

--- aggregator/views.py
+++ aggregator/views.py
@@ -15,6 +15,7 @@ class PostListView(ListView):
     template_name = 'aggregator/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 30
 
 class PostDetailView(DetailView):
     model = Post
